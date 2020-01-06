834c01ce302c0fa21386508ec74ae41c65cf11c4
fflorek <fflorek.7@gmail.com>
2020-01-06 14:09:25 +0100

    first fronend commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => {
+  return <div>
+    Hello world!
+  </div>
+}
+
+
+export default Home
