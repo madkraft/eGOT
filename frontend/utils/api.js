a55bc789351700eba7a669451ebd42368ccd5bdd
fflorek <fflorek.7@gmail.com>
2020-01-12 17:53:29 +0100

    deleted next

--- frontend/utils/api.js
+++
@@ -1,9 +0,0 @@
-import fetch from "isomorphic-unfetch"
-
-export const fetchMountainRanges = () => {
-    return fetch('http://localhost:8080/api/mountains')
-        .then((response) => {
-            return response.json();
-        });
-
-}
